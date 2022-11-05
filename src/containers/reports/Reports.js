e0d033284e6503ae21da75f2ff4a4304b7acc70a
Chí Hiếu <hieumeo171@gmail.com>
2022-11-05 14:49:17 +0700

    finish

--- src/containers/reports/Reports.js
+++ src/containers/reports/Reports.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Stack } from 'react-bootstrap';
 import Navbar from '../../components/sidebar/Navbar';
 import { useBudgets } from '../../contexts/BudgetsContext';
 import LineChart from "./LineChart"
@@ -8,17 +9,18 @@ function Reports() {
   const expenses = useBudgets().expenses
   const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 
-  console.log(expenses[0].date.substring(5,7) + "!!!!!!!!!!!")
   let monthTotalExpenses = []
-  for(let i = 0; i < months.lenth; i++) {
+  for(let i = 0; i < months.length; i++) {
     let total = 0
     for(let j = 0; j < expenses.length; j++) {
-      if(expenses[j].date.substring(5,7) === months[i]) {
+      if(expenses[j].date.substring(5,7) === months[i]) {      
         total += expenses[j].amount
       }
     }
+    console.log(total)
     monthTotalExpenses.push(total)
   }
+  console.log(monthTotalExpenses)
   
   const [data, setData] = useState({
     labels: months,
@@ -43,14 +45,14 @@ function Reports() {
   return (
     <>
       <Navbar />
-      <div>
+      <Stack direction="horizontal">
         <div style={{ width: 700 }}>
           <LineChart chartData={data} />
         </div>
         <div style={{ width: 700 }}>
           <PieChart chartData={data} />
         </div>
-      </div>
+      </Stack>
     </>
   );
 }
